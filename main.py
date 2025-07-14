e8e004cf4c03e4752727aa3581101b7b96ab1b9a
sarahbarberuk <sarahbarberuk@gmail.com>
2025-07-14 18:04:13 +0100

    Added a frontend for uploading multiple pdfs and asking questions about the ldfs

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI, UploadFile, File, BackgroundTasks
 from pydantic import BaseModel
 from rag_chain import get_rag_chain
 from ingest import ingest
+from typing import List
 import shutil
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 qa = get_rag_chain()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 class Query(BaseModel):
     question: str
+
+@app.get("/", response_class=HTMLResponse)
+def serve_upload_page():
+    with open("static/upload.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(content=f.read())
 
 @app.post("/ask")
 def ask_question(query: Query):
@@ -19,17 +29,26 @@ def ask_question(query: Query):
     }
 
 @app.post("/upload")
-async def upload_pdf(
-        file: UploadFile = File(...),
-        background_tasks: BackgroundTasks = BackgroundTasks()
+async def upload_pdfs(
+        background_tasks: BackgroundTasks,
+        files: List[UploadFile] = File(...)
+
     ):
-    # Uploads to data/ folder
-    file_path = f"data/{file.filename}"
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(file.file, f)
+    uploaded = []
 
-    # Ingests to vectordb/
-    # Run in the background (non-blocking)
-    background_tasks.add_task(ingest, file_path)
+    for file in files:
+        # Uploads to data/ folder
+        file_path = f"data/{file.filename}"
+        with open(file_path, "wb") as f_out:
+            shutil.copyfileobj(file.file, f_out)
 
-    return {"status": "uploaded", "filename": file.filename}
+        # Ingests to vectordb/
+        # Run in the background (non-blocking)
+        background_tasks.add_task(ingest, file_path)
+
+        uploaded.append(file.filename)
+
+    return {
+        "status": "uploaded",
+        "message": f"Uploaded and started ingesting: {', '.join(uploaded)}"
+    }
